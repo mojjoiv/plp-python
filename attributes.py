36f572994408355f9a645d86171494d2c2b99a78
Patrick Ongeche <daltonongeche@gmail.com>
2024-04-01 15:28:03 +0400

    Create attributes.py

---
+++ attributes.py
@@ -0,0 +1,12 @@
+class Person:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+    
+    def introduce(self):
+        print(f"Hello, my name is {self.name}. I am {self.age} years old and my gender is {self.gender}.")
+
+person1 = Person("Patrick", 27, "male")
+
+person1.introduce()
